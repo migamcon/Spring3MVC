f9502385efbf8527f79c9219afe1496e959d371b
migamcon <57899065+migamcon@users.noreply.github.com>
2019-11-20 13:47:16 +0100

    Create Deploy.bat

---
+++ Deploy.bat
@@ -0,0 +1,2 @@
+echo "DESPLEGANDO EN DESARROLLO"
+deploy adapters: [tomcat8(credentialsId: '7053c9c1-6a58-4f0c-8215-499382178ad0', path: '', url: 'http://localhost:8081/')], contextPath: null, onFailure: false, war: '**/*.war'
