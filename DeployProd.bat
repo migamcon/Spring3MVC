9099d0416fcaf3057b21dca0bcbb0804ad7e9a02
migamcon <57899065+migamcon@users.noreply.github.com>
2019-11-20 13:59:13 +0100

    Update DeployProd.bat

--- DeployProd.bat
+++ DeployProd.bat
@@ -1,2 +1,2 @@
 echo "DESPLEGANDO EN DESARROLLO"
-deploy adapters: [tomcat8(credentialsId: '7053c9c1-6a58-4f0c-8215-499382178ad0', path: '', url: 'http://localhost:8083/')], contextPath: null, onFailure: false, war: '**/*.war'
+deploy adapters: [tomcat8(credentialsId: '7053c9c1-6a58-4f0c-8215-499382178ad0', path: '', url: 'http://localhost:8083/webapp')], contextPath: null, onFailure: false, war: '**/*.war'
